61dc2f347d7feb98589c0788ae892967b3b46a9a
Subash Jirel <subashjrl@gmail.com>
2024-05-15 23:06:43 +0545

    initialized files and added db

---
+++ app/blogs/[id]/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function BlogDetails({params}) {
+  return (
+    <div>BlogDetails Number {params.id}</div>
+  )
+}
+
+export default BlogDetails
---
+++ app/blogs/add-blog/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function AddBlogPage() {
+  return <div>AddBlogPage</div>;
+}
+
+export default AddBlogPage;
